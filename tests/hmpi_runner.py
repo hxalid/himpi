2ce6601f8522c154583fdb5fc63a92347f03190f
Khalid Hasanov <khalid.hasanov@ucdconnect.ie>
2015-05-27 12:54:08 +0100

    use only half of the group numbers as it is symmetric in regards to number of processes per group

--- tests/hmpi_runner.py
+++ tests/hmpi_runner.py
@@ -30,12 +30,9 @@ class HMPI_Runner(object):
                 fout.write(str(num_comm_size) + '\n')
                 for cs in comm_sizes:
                     fout.write(str(cs) + '\n')
-                    print(comm_msg_sizes)
-                    print('cs=', cs)
                     num_msg_sizes = len(comm_msg_sizes.get(str(cs)))
                     fout.write(str(num_msg_sizes) + '\n')
                     msg_list = comm_msg_sizes.get(str(cs))
-                    print('msg_list=', msg_list)
                     for m in msg_list:
                         fout.write(m + '\n')
             
@@ -46,33 +43,37 @@ class HMPI_Runner(object):
 if __name__ == '__main__':
     p = 16
     
-    coll_ids = ['7']
-    msg_sizes_g = []
-    msg_sizes_pg = []    
-    
-    for g in range(1, p):
-        comm_msgs = {}
+    alg_count = 7
+    alg_id = '7'
+    coll_ids = [alg_id] 
+    msg_sizes= [0]
+    msg_sizes.extend([2**m for m in range(0, 17, 2)])
+        
+    for g in range(1, p//2):
         if (p%g==0):
-            for alg_in in range(0, 7):
-                for alg_out in range(0, 7):
+            for alg_in in range(0, alg_count):
+                for alg_out in range(0, alg_count):
                     file_name = './silgetsin/ompi_conf_g%s_alg_in%s_alg_out%s' % (g, alg_in, alg_out)
                     
+                    comm_msgs = {}
+                    msg_sizes_g = []
+                    msg_sizes_pg = []   
                     comm_sizes = []
-                    if (g>p/g):
-                        comm_sizes.append(int(p/g))
+                    
+                    if (g>p//g):
+                        comm_sizes.append(p//g)
                         comm_sizes.append(g)
                     else:
                         comm_sizes.append(g)
-                        comm_sizes.append(int(p/g))
+                        comm_sizes.append(p//g)
                     
-                    
-                    for m in [2**m for m in range(0, 17, 2)]:
+                    for m in msg_sizes:
                         msg_sizes_pg.append('%s %s %s %s' % (m, alg_in, 0, 0))
                         msg_sizes_g.append('%s %s %s %s' % (m, alg_out, 0, 0))
                     
                     
                     comm_msgs[str(g)] = msg_sizes_g
-                    comm_msgs[str(int(p/g))] = msg_sizes_pg
+                    comm_msgs[str(p//g)] = msg_sizes_pg
                     
                     
                     HMPI_Runner.create_config_file(file_name, coll_ids, comm_sizes, comm_msgs)
@@ -81,9 +82,9 @@ if __name__ == '__main__':
 
 
 # [message_size alg topo segmentation] 
-    msg_sizes_p0 = ['0 1 0 0', '1024 0 0 0', '8192 6 0 0', '16384 5 0 0', '32768 6 0 0', '262144 3 0 0', '524288 4 0 0']
-    msg_sizes_p16 = ['0 1 0 0', '1024 6 0 0', '8192 6 0 0', '16384 5 0 0', '32768 2 0 0', '262144 3 0 0', '524288 4 0 0']
-    HMPI_Runner.create_config_file('fayildi-bu', ['7'], ['0', '16'], {'0':msg_sizes_p0, '16':msg_sizes_p16})
+#    msg_sizes_p0 = ['0 1 0 0', '1024 0 0 0', '8192 6 0 0', '16384 5 0 0', '32768 6 0 0', '262144 3 0 0', '524288 4 0 0']
+#    msg_sizes_p16 = ['0 1 0 0', '1024 6 0 0', '8192 6 0 0', '16384 5 0 0', '32768 2 0 0', '262144 3 0 0', '524288 4 0 0']
+#    HMPI_Runner.create_config_file('fayildi-bu', [alg_id], ['0', '16'], {'0':msg_sizes_p0, '16':msg_sizes_p16})
                     
                     
                     
